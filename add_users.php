c6845165c6bfb7ce03454d0c3c779acb3a81e0a1
Jatin Singh Satija <67864252+jatinsinghsatija@users.noreply.github.com>
2025-07-16 16:40:37 +0530

    Update add_users.php

--- add_users.php
+++ add_users.php
@@ -1,6 +1,12 @@
 <?php
 header("Content-Type: application/json");
 
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405); // Method Not Allowed
+    echo json_encode(["status" => "error", "message" => "Only POST method is allowed"]);
+    exit;
+}
+
 $file = 'users.json';
 
 // Load current users
